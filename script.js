3be56381fb077f745d2f4a2c140ad89d9ae55664
EvvTim <evgeniytim94@gmail.com>
2020-02-29 19:59:04 +0100

    hamburger-menu

---
+++ script.js
@@ -0,0 +1,7 @@
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('.navigation');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('hamburger--active');
+    nav.classList.toggle('navigation--active')
+})
